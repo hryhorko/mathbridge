aed61ddf9cc42833174765c1c652c5ec27a9c3b9
hryhorko <118362693+hryhorko@users.noreply.github.com>
2026-08-09 12:26:19 +0300

    Create module.sql

---
+++ atutor/jupyter_auth/module.sql
@@ -0,0 +1,61 @@
+# Jupyter Authentication module language strings
+
+INSERT INTO `language_text`
+    VALUES (
+        'en',
+        '_module',
+        'jupyter_auth',
+        'JWT Authorization',
+        NOW(),
+        'JupyterHub JWT Authorization module'
+    );
+
+INSERT INTO `language_text`
+    VALUES (
+        'uk',
+        '_module',
+        'jupyter_auth',
+        'JWT авторизація',
+        NOW(),
+        'Модуль JWT-авторизації JupyterHub'
+    );
+
+INSERT INTO `language_text`
+    VALUES (
+        'en',
+        '_module',
+        'jupyter_auth_desc',
+        'Launch JupyterHub with secure JWT-based authentication.',
+        NOW(),
+        'JupyterHub JWT Authorization description'
+    );
+
+INSERT INTO `language_text`
+    VALUES (
+        'uk',
+        '_module',
+        'jupyter_auth_desc',
+        'Запуск JupyterHub через JWT-авторизацію.',
+        NOW(),
+        'Опис модуля JWT-авторизації JupyterHub'
+    );
+
+INSERT INTO `language_text`
+    VALUES (
+        'en',
+        '_module',
+        'launch_jupyterhub',
+        'Open JupyterHub',
+        NOW(),
+        'JupyterHub launch link'
+    );
+
+INSERT INTO `language_text`
+    VALUES (
+        'uk',
+        '_module',
+        'launch_jupyterhub',
+        'Відкрити JupyterHub',
+        NOW(),
+        'Посилання для запуску JupyterHub'
+    );
